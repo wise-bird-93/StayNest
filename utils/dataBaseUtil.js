27f38ae121657ea1fa3f3c61a8bbfb37c78ee205
wise-bird-93 <yuvrajdhingra303@gmail.com>
2026-06-26 12:59:40 +0530

    Fix MySQL SSL connection for production

--- utils/dataBaseUtil.js
+++ utils/dataBaseUtil.js
@@ -3,7 +3,11 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: 22990,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool.promise();
